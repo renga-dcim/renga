bf881842a7ff4532c04ed663c5636a0f3d281977
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-29 16:52:50 -0700

    setup flake

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "Renga - DCIM";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rust-toolchain = pkgs.rust-bin.stable."1.96.0".default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+            "rustfmt"
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Elixir
+            beam.packages.erlang_28.elixir_1_20
+            beam.packages.erlang_28.rebar3
+            erlang_28
+
+            # Rust
+            rust-toolchain
+
+            # LSPs
+            beamPackages.expert
+            erlang-language-platform
+            rust-analyzer
+            yaml-language-server
+
+            # Tools
+            watchman
+            docker-compose
+            yamllint
+            pkg-config
+            openssl
+            shfmt
+            shellcheck
+            git-cliff
+            postgresql
+          ];
+
+          shellHook = ''
+            export MIX_HOME="$PWD/.nix/mix"
+            export HEX_HOME="$PWD/.nix/hex"
+            export REBAR_CACHE_DIR="$PWD/.nix/rebar3"
+            export ERL_AFLAGS="-kernel shell_history enabled"
+
+            mkdir -p "$MIX_HOME" "$HEX_HOME" "$REBAR_CACHE_DIR"
+          '';
+
+        };
+      }
+    );
+}
