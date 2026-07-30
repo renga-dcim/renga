8ec7299801832e75cbd3373a475fae8bb9b18884
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-30 09:54:29 -0700

    change dev ports

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ import Config
 
 # Configure your database
 config :renga, Renga.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "renga_dev",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASSWORD") || "postgres",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  port: String.to_integer(System.get_env("DATABASE_PORT") || "5435"),
+  database: System.get_env("DATABASE_NAME") || "renga_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +20,7 @@ config :renga, Renga.Repo,
 config :renga, RengaWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4401")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
