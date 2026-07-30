53693a41026505bc3f8d478778e88484dff9cae7
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-30 15:18:52 -0700

    Align test database port with compose

--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,10 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :renga, Renga.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASSWORD") || "postgres",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  port: String.to_integer(System.get_env("DATABASE_PORT") || "5435"),
   database: "renga_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
