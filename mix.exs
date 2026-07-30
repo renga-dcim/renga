bbc78bf2e39366b06b6b5fbaa71b995f6d102569
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-30 09:58:02 -0700

    setup CI

--- mix.exs
+++ mix.exs
@@ -65,7 +65,10 @@ defmodule Renga.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+
+      # dev or test
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
