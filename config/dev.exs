699f325601c9c37b529aa35e2bf835a3daf39e64
Darwin Wu <tough.pasta0068@wunli88.com>
2026-08-09 07:35:32 +0000

    chore: prepare Amp orb environment

--- config/dev.exs
+++ config/dev.exs
@@ -18,9 +18,8 @@ config :renga, Renga.Repo,
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
 config :renga, RengaWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4401")],
+  # Access is limited to authorized tailnet peers by the Tailscale policy.
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
